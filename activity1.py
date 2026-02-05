99644cc1d3e81206dad35492fcbcc7cb2c0615ea
Ahnaf-The-Coder12 <ashrafahnafayaan@gmail.com>
2026-02-05 07:19:18 -0800

    Add files via upload

--- activity1.py
+++ activity1.py
@@ -1,4 +1,9 @@
-def fun1(n):
-    return (n*(n+1))/2
+def printnumber(n):
+    iteration=0
+    print("The number entered by user is ",n)
+    iteration+=1
+    print("Total iterations done by the code is ",iteration,"\n")
 
-print (fun1(4))
+printnumber(10)
+printnumber(20)
+print("\n With any 'n' the time taken by our code wont change")
